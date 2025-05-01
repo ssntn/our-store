1b58e873d101fda694f2abc605cc2d6719c2139d
ssntn <iansisante@gmail.com>
2025-05-01 17:29:26 +0800

    EDIT: get by id, all, pagination

--- api/views/products.py
+++ api/views/products.py
@@ -10,17 +10,48 @@ from api.model.product import Product as p
 from ..serializers import ProductSerializer
 
 @api_view(['GET'])
-def get(request, id):
-  
+def get(request, id=None, page_size=None):
   if id:
-    product = get_object_or_404(p, pk=id)
-    serializer = ProductSerializer(product)
-    return Response(serializer.data)
-
-  productList = p.objects.all()
-  serializer = ProductSerializer(productList, many=True)
-  return Response(serializer.data)
+    return get_by_id(id)
+  
+  page_size = request.GET.get('page_size')
+  if page_size:
+    page = int(request.GET.get('page', 1))
+    return get_paginated(int(page_size), int(page))
+  
+  return get_all()
+ 
 
 @api_view(['POST'])
 def create(request):
-  pass
+    serializer = ProductSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=201)
+    return Response(serializer.errors, status=400)
+
+#region PRIVATES
+def get_by_id(id):
+  product = get_object_or_404(p, pk=id)
+  serializer = ProductSerializer(product)
+  return Response(serializer.data)   
+
+def get_paginated(page_size, page):
+
+  start = (page - 1) * page_size
+  end = start + page_size
+
+  queryset = p.objects.all()[start:end]
+  serializer = ProductSerializer(queryset, many=True)
+
+  return Response({
+      'page': page,
+      'page_size': page_size,
+      'results': serializer.data
+  })
+
+def get_all():
+  productList = p.objects.all()
+  serializer = ProductSerializer(productList, many=True)
+  return Response(serializer.data)
+#endregion 
