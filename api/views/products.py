b551fad4765ccca6266f793d9441016a658133c2
ssntn <iansisante@gmail.com>
2025-05-01 18:01:22 +0800

    ADD: create + delete on database

--- api/views/products.py
+++ api/views/products.py
@@ -9,8 +9,16 @@ from rest_framework import status
 from api.model.product import Product as p
 from ..serializers import ProductSerializer
 
-@api_view(['GET'])
+@api_view(['GET', 'POST', 'DELETE'])
 def get(request, id=None, page_size=None):
+  
+  # Non get methods
+  if request.method == 'POST':
+    return create(request)
+  elif request.method == 'DELETE':
+     return delete(id)
+
+  # Get
   if id:
     return get_by_id(id)
   
@@ -21,14 +29,24 @@ def get(request, id=None, page_size=None):
   
   return get_all()
  
-
-@api_view(['POST'])
 def create(request):
     serializer = ProductSerializer(data=request.data)
     if serializer.is_valid():
         serializer.save()
-        return Response(serializer.data, status=201)
-    return Response(serializer.errors, status=400)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
+    print(request.data)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+def delete(id):
+    try:
+        product = p.objects.get(pk=id, is_deleted=False)
+        product.is_deleted = True
+        product.save()
+        return Response({"detail": "Product deleted."}, status=status.HTTP_204_NO_CONTENT)
+    except p.DoesNotExist:
+        return Response({"detail": "Product not found."}, status=status.HTTP_400_BAD_REQUEST)
+
 
 #region PRIVATES
 def get_by_id(id):
@@ -41,7 +59,7 @@ def get_paginated(page_size, page):
   start = (page - 1) * page_size
   end = start + page_size
 
-  queryset = p.objects.all()[start:end]
+  queryset = p.objects.filter(is_deleted=False)[start:end]
   serializer = ProductSerializer(queryset, many=True)
 
   return Response({
